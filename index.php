b74e9e6ca3ea632cb9a715580716b883ac7a222e
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2021-07-12 08:12:09 -0300

    estrutura inicial

--- index.php
+++ index.php
@@ -2,5 +2,21 @@
 
 require 'vendor/autoload.php';
 
+use League\Csv\Reader;
+use League\Csv\Statement;
+use League\Csv\CharsetConverter;
+
+//load the CSV document from a stream
+$stream = fopen('assets/csv/questões_wordpress.csv', 'r');
+$csv = Reader::createFromStream($stream);
+$csv->setDelimiter(';');
+$csv->setHeaderOffset(0);
+
+$stmt = Statement::create();   
+$records = $stmt->process($csv);
+
+foreach ($records as $index=>$record) {    
+    echo $record['title'] . PHP_EOL; 
+}
 
 ?>
