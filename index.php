726c3e83367927e4a9a55d1be3106f9793533cdb
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2021-07-13 15:03:16 -0300

    processo de quetoes csv

--- index.php
+++ index.php
@@ -7,14 +7,17 @@ use League\Csv\Writer;
 
 date_default_timezone_set('America/Sao_Paulo');
 
-$csv = new CsvGenerator();
+$csv = new CsvGenerator(); 
  
-$records = [];
 $csvData = $csv->processCsvData();
-$records[] = $csvData;
 
-$csv_questions_download = Writer::createFromFileObject(new SplTempFileObject());
-$csv_questions_download->insertOne(['identificador','enunciated','link_do_video','alternativa_a', 'alternativa_b','alternativa_c','alternativa_d','alternativa_e']);
-$csv_questions_download ->insertAll($records);
-$csv_questions_download->output('questions'.date('d-m-Y H:i:s', time()).'.csv');
- ?>
+// try {
+//     $csv_questions_download = Writer::createFromFileObject(new SplTempFileObject());
+//     $csv_questions_download->insertOne(['identificador','enunciated','link_do_video','alternativa_a', 'alternativa_b','alternativa_c','alternativa_d','alternativa_e']);
+//     $csv_questions_download ->insertAll($csvData);
+//     $csv_questions_download->output('questions'.date('d-m-Y H:i:s', time()).'.csv');
+// } catch (Exception | RuntimeException $e) {
+//     echo $e->getMessage(), PHP_EOL;
+// }
+
+?>
--- src/CsvGenerator.php
+++ src/CsvGenerator.php
@@ -20,38 +20,38 @@ class CsvGenerator {
         $stmt = Statement::create();   
         $records = $stmt->process($csv);
 
-        $answer_data = array(
-            'identificador' => '',
-            'enunciated' => '',
-            'video' => '',  
-            'alternative_a' =>[ 'a','b','c','d','e']      
-        );
 
-        foreach ($records as $index=>$record) {  
+        $questions_data = array();
+
+        foreach ($records as $index=>$record) { 
+            
+            $answer_data = array(
+                'identificador'=> '',
+                'enunciated'   => '',
+                'video'        => '',
+                'alternatives' =>''                     
+            );
 
             $answer_data['identificador'] = $record['title'];
             $answer_data['enunciated']    = $record['question'];
             $answer_data['video']         = $record['correct_msg'];
 
-            // $fixed_data = preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {      
-            //     return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
-            // },str_replace(array('<p>','</p>','*'),'',$record['answer_data']));
+            $fixed_data = preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {      
+                return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
+            },str_replace(array('<p>','</p>','*'),'',$record['answer_data']));
 
-            // $alternatives = json_decode(json_encode(unserialize($fixed_data)));
- 
-            // foreach($alternatives as $alternative){
-            //         // if($alternative->_answer == '') {
-            //         //     continue;
-            //         // }
-            //         // echo $alternative->_answer.PHP_EOL;
-            //     @array_push($answer_data, $alternative->_answer);
-            // }
+            @$alternatives = json_decode(json_encode(unserialize($fixed_data))); 
+            
+            foreach($alternatives as $alternative){
+                if($alternative->_answer == '') {
+                    continue;
+                }
+                $answer_data['alternatives'] =  $alternative->_answer;
+            }
+            array_push($questions_data,$answer_data);
         }
- 
-        return $answer_data;        
-    }
- 
-    
+        return $questions_data;        
+    } 
 }
 
 ?>
