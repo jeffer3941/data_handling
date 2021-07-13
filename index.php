b412747d6d252270ad566fa435e28adf74e48760
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2021-07-13 12:18:03 -0300

    criacao de csv

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
 require 'vendor/autoload.php';
 
 use emr\tratamentoCsv\CsvGenerator;
+use League\Csv\Writer;
+
+date_default_timezone_set('America/Sao_Paulo');
 
 $csv = new CsvGenerator();
+ 
+$records = [];
+$csvData = $csv->processCsvData();
+$records[] = $csvData;
 
-
-// $csv_questions_download = Writer::createFromFileObject(new SplTempFileObject());
-// $csv_questions_download->insertOne(['identificador', 'alternativa_a', 'alternativa_b','alternativa_c','alternativa_d','alternativa_e','enunciated']);
-// $csv_questions_download->output('questions.csv');
-// exit();
-// echo 'error: ', json_last_error_msg(). PHP_EOL;
-?>
+$csv_questions_download = Writer::createFromFileObject(new SplTempFileObject());
+$csv_questions_download->insertOne(['identificador','enunciated','link_do_video','alternativa_a', 'alternativa_b','alternativa_c','alternativa_d','alternativa_e']);
+$csv_questions_download ->insertAll($records);
+$csv_questions_download->output('questions'.date('d-m-Y H:i:s', time()).'.csv');
+ ?>
--- src/CsvGenerator.php
+++ src/CsvGenerator.php
@@ -3,15 +3,13 @@
 namespace emr\tratamentoCsv;
 
 use League\Csv\Reader;
-use League\Csv\Statement;
 use League\Csv\Writer;
+use League\Csv\Statement;
+ 
 
 class CsvGenerator {
-    public function __construct() {
-        $this->processCsvData();
-    }
-
-    private function processCsvData()
+ 
+    public function processCsvData() : array 
     {
         $stream = fopen('assets/csv/questões_wordpress.csv', 'r');
         $csv = Reader::createFromStream($stream);
@@ -22,27 +20,37 @@ class CsvGenerator {
         $stmt = Statement::create();   
         $records = $stmt->process($csv);
 
-        $answer_data = array();
+        $answer_data = array(
+            'identificador' => '',
+            'enunciated' => '',
+            'video' => '',  
+            'alternative_a' =>[ 'a','b','c','d','e']      
+        );
 
-        foreach ($records as $index=>$record) {   
-            array_push($answer_data,str_replace(array('<p>','</p>','*'),'',$record['answer_data']));
+        foreach ($records as $index=>$record) {  
+
+            $answer_data['identificador'] = $record['title'];
+            $answer_data['enunciated']    = $record['question'];
+            $answer_data['video']         = $record['correct_msg'];
+
+            // $fixed_data = preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {      
+            //     return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
+            // },str_replace(array('<p>','</p>','*'),'',$record['answer_data']));
+
+            // $alternatives = json_decode(json_encode(unserialize($fixed_data)));
+ 
+            // foreach($alternatives as $alternative){
+            //         // if($alternative->_answer == '') {
+            //         //     continue;
+            //         // }
+            //         // echo $alternative->_answer.PHP_EOL;
+            //     @array_push($answer_data, $alternative->_answer);
+            // }
         }
-
-        $fixed_data = preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {      
-            return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
-        },$answer_data);
-
-        $alternatives = json_decode(json_encode(unserialize($fixed_data)));
-
-
-        foreach($alternatives as $alternative){
-            if($alternative->_answer == '') {
-                continue;
-            }
-            echo $alternative->_answer.PHP_EOL;
-        }
-
+ 
+        return $answer_data;        
     }
+ 
     
 }
 
