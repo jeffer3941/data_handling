68a47ce66c3a04a8b28a15e9124b5b652f22879f
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2021-07-14 13:57:45 -0300

    conclusao de projeto

--- index.php
+++ index.php
@@ -10,14 +10,13 @@ date_default_timezone_set('America/Sao_Paulo');
 $csv = new CsvGenerator(); 
  
 $csvData = $csv->processCsvData();
-
-// try {
-//     $csv_questions_download = Writer::createFromFileObject(new SplTempFileObject());
-//     $csv_questions_download->insertOne(['identificador','enunciated','link_do_video','alternativa_a', 'alternativa_b','alternativa_c','alternativa_d','alternativa_e']);
-//     $csv_questions_download ->insertAll($csvData);
-//     $csv_questions_download->output('questions'.date('d-m-Y H:i:s', time()).'.csv');
-// } catch (Exception | RuntimeException $e) {
-//     echo $e->getMessage(), PHP_EOL;
-// }
-
-?>
+ 
+try {
+    $csv_questions_download = Writer::createFromFileObject(new SplTempFileObject());
+    $csv_questions_download->insertOne(['identificador','enunciated','link_do_video','alternativa_a', 'alternativa_b','alternativa_c','alternativa_d','alternativa_e','correct']);
+    $csv_questions_download ->insertAll($csvData);
+    $csv_questions_download->output('questions '.date('d-m-Y H:i:s', time()).'.csv');
+} catch (Exception | RuntimeException $e) {
+    echo $e->getMessage(), PHP_EOL;
+}
+ 
--- src/CsvGenerator.php
+++ src/CsvGenerator.php
@@ -5,11 +5,9 @@ namespace emr\tratamentoCsv;
 use League\Csv\Reader;
 use League\Csv\Writer;
 use League\Csv\Statement;
- 
-
-class CsvGenerator {
- 
-    public function processCsvData() : array 
+class CsvGenerator
+{ 
+    public function processCsvData( ) 
     {
         $stream = fopen('assets/csv/questões_wordpress.csv', 'r');
         $csv = Reader::createFromStream($stream);
@@ -17,41 +15,50 @@ class CsvGenerator {
         $csv->setDelimiter(';');
         $csv->setHeaderOffset(0);
 
-        $stmt = Statement::create();   
+        $stmt = Statement::create();
         $records = $stmt->process($csv);
 
 
         $questions_data = array();
 
-        foreach ($records as $index=>$record) { 
-            
+        foreach ($records as $index => $record) {   
+            $alternatives = array();
             $answer_data = array(
-                'identificador'=> '',
-                'enunciated'   => '',
-                'video'        => '',
-                'alternatives' =>''                     
+                'identificador' => '',
+                'enunciated'    => '',
+                'video'         => '',
+                'alternative_a' => '',
+                'alternative_b' => '',
+                'alternative_c' => '',
+                'alternative_d' => '',
+                'alternative_e' => '',
+                'correct'       => '',                   
             );
 
             $answer_data['identificador'] = $record['title'];
             $answer_data['enunciated']    = $record['question'];
             $answer_data['video']         = $record['correct_msg'];
-
+ 
             $fixed_data = preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {      
                 return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
-            },str_replace(array('<p>','</p>','*'),'',$record['answer_data']));
-
-            @$alternatives = json_decode(json_encode(unserialize($fixed_data))); 
+            },str_replace('*','',$record['answer_data']));            
+ 
+            $alternatives = json_decode(json_encode(@unserialize($fixed_data)),true);  
             
-            foreach($alternatives as $alternative){
-                if($alternative->_answer == '') {
-                    continue;
-                }
-                $answer_data['alternatives'] =  $alternative->_answer;
-            }
-            array_push($questions_data,$answer_data);
-        }
-        return $questions_data;        
-    } 
-}
+ 
+            if ($alternatives !== false) {
+                $alternative_title_collum = ['alternative_a','alternative_b','alternative_c','alternative_d','alternative_e'];
+                $alternative_correct_value = ['A','B','C','D','E'];
 
-?>
+                foreach ($alternatives as $index => $alternative){ 
+                    if($alternative['_correct'] == 1){
+                        $answer_data['correct'] = $alternative_correct_value[$index];
+                    }
+                    @$answer_data[$alternative_title_collum[$index]] = $alternative['_answer'];                  
+                }
+            }     
+            array_push($questions_data,$answer_data);      
+        }
+        return $questions_data;            
+    }  
+}
